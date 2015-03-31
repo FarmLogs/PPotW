39133538044c294184abc3a731c274a8597efda8
johnny313 <john@farmlogs.com>
2015-03-30 21:41:57 -0400

    opps

--- 1-TotallyNotFizzBuzz/solutions/fizz_buzzy_buzz.py
+++ 1-TotallyNotFizzBuzz/solutions/fizz_buzzy_buzz.py
@@ -1,3 +1,4 @@
+#!/opt/local/bin/python
 
 f = ['fizz']+['']*4
 b = ['buzz']+['']*4
@@ -5,5 +6,6 @@ nums = range(1,101)
 fizz = map(lambda x: f[x%3],nums)
 buzz = map(lambda x: b[x%5],nums)
 fizzbuzz = map(lambda (x,y): ''.join((x,y)), zip(fizz,buzz))
-[max([n,f], key=len) for n,f in zip(map(str,nums), fizzbuzz)]
 
+for el in [max([n,f], key=len) for n,f in zip(map(str,nums), fizzbuzz)]:
+    print el
