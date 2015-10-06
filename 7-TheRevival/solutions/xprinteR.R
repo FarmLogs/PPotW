f42602a218206822f854008b135dc2593aed56ab
Ellis Valentiner <ellis@farmlogs.com>
2015-10-06 14:02:06 -0400

    rotate matrix and ascii print util functions

--- 7-TheRevival/solutions/xprinteR.R
+++ 7-TheRevival/solutions/xprinteR.R
@@ -2,6 +2,18 @@
 # Author: Ellis Valentiner
 # Date: 2015-10-06
 
+rotate.matrix <- function(m, direction=c('ccw', 'cw')){
+  direction <- match.arg(direction)
+  switch(direction,
+         ccw = apply(m, 1, rev),
+         cw = t(apply(m, 2, rev)))
+}
+
+ascii_print <- function(m){
+  write.table(format(m, justify="centre"),
+              sep='', row.names=FALSE, col.names=FALSE, quote=FALSE)
+}
+
 # xprinterR
 #   Function to ascii print NxN size X
 #
@@ -17,13 +29,12 @@ xprinteR <- function(n=5L){
   # Input must be an odd integer
   stopifnot(n == as.integer(n), n%%2==1)
   # Create bidirectional indicator matrix
-  X <- diag(1, n, n) + t(apply(diag(1, n, n), 2, rev))
+  X <- diag(1, n, n) + rotate.matrix(diag(1, n, n))
   # Substitution
   X[X >= 1] <- 'X'
   X[X == 0] <- ' '
   # Print
-  write.table(format(X, justify="centre"),
-              sep='', row.names=FALSE, col.names=FALSE, quote=FALSE)
+  ascii_print(X)
 }
 
 # dprinterR
@@ -40,15 +51,14 @@ xprinteR <- function(n=5L){
 dprinteR <- function(n=5L){
   # Input must be an odd integer
   stopifnot(n == as.integer(n), n%%2==1)
-  p <- (n - 1)/2
+  p <- ((n - 1) / 2) + 1
   # Create bidirectional indicator matrix
   A <- diag(1, p, p)
-  B <- t(apply(A, 2, rev))
+  B <- rotate.matrix(A)
   X <- rbind(cbind(B, A[,-1]), cbind(A, B[,-1])[-1,])
   # Substitution
   X[X >= 1] <- 'X'
   X[X == 0] <- ' '
   # Print
-  write.table(format(X, justify="centre"),
-              sep='', row.names=FALSE, col.names=FALSE, quote=FALSE)
+  ascii_print(X)
 }
