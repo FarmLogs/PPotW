daeb59a61c9b069960ab60de540b18641449faf7
Ellis Valentiner <ellis@farmlogs.com>
2015-10-06 13:32:30 -0400

    diamond function

--- 7-TheRevival/solutions/xprinteR.R
+++ 7-TheRevival/solutions/xprinteR.R
@@ -1,7 +1,7 @@
 # PPoTW 7
 # Author: Ellis Valentiner
 # Date: 2015-10-06
-#
+
 # xprinterR
 #   Function to ascii print NxN size X
 #
@@ -18,6 +18,33 @@ xprinteR <- function(n=5L){
   stopifnot(n == as.integer(n), n%%2==1)
   # Create bidirectional indicator matrix
   X <- diag(1, n, n) + t(apply(diag(1, n, n), 2, rev))
+  # Substitution
+  X[X >= 1] <- 'X'
+  X[X == 0] <- ' '
+  # Print
+  write.table(format(X, justify="centre"),
+              sep='', row.names=FALSE, col.names=FALSE, quote=FALSE)
+}
+
+# dprinterR
+#   Function to ascii print NxN size diamond
+#
+# Example
+# > myfantasticXprinter(5)
+#   X  
+#  X X 
+# X   X
+#  X X 
+#   X  
+#
+dprinteR <- function(n=5L){
+  # Input must be an odd integer
+  stopifnot(n == as.integer(n), n%%2==1)
+  p <- (n - 1)/2
+  # Create bidirectional indicator matrix
+  A <- diag(1, p, p)
+  B <- t(apply(A, 2, rev))
+  X <- rbind(cbind(B, A[,-1]), cbind(A, B[,-1])[-1,])
   # Substitution
   X[X >= 1] <- 'X'
   X[X == 0] <- ' '
