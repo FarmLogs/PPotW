f0c8bd4455c19685f00711771a5f43e17578e376
Frank Burnham <frank.e.burnham@gmail.com>
2015-10-21 07:04:33 -0400

    Those beautiful parens

---
+++ 8-Parens/solutions/beautiful-parens/test/beautiful_parens/core_test.clj
@@ -0,0 +1,9 @@
+(ns beautiful-parens.core-test
+  (:require [clojure.test :refer :all]
+            [beautiful-parens.core :refer :all]))
+
+(deftest test-beautiful-parens?
+  (are [input expected] (= expected (beautiful-parens? input))
+       "()()()([''][])" true
+       "(((()()'')))" true
+       "((((()()')')))" false))
