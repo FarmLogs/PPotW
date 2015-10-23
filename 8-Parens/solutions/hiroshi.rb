7a325f2ff5d13510c38ac801ac285944b08446c8
Hiroshi Fukada <hiroshi@fukada.us>
2015-10-23 00:02:19 -0400

    Update hiroshi.rb
    oops.

--- 8-Parens/solutions/hiroshi.rb
+++ 8-Parens/solutions/hiroshi.rb
@@ -3,7 +3,7 @@ def eat(a, char)
 end
 def wow(r)
   ->s{!s.gsub(/#{[40,124,41,124,123,124,125,124,91,124,93].pack('c'*11)}/,String.new)[/./] ? s.unpack('c'*s.length).inject(s.length){|accumulator, char| eat(accumulator, char)} == s.length : false}\
-    [r.gsub(/#{[39,46,42,63,39].pack('C',5)}/,String.new)]
+    [r.gsub(/#{[39,46,42,63,39].pack('C'*5)}/,String.new)]
 end
 
 puts "true: #{wow("()()()([')'][])")}"
