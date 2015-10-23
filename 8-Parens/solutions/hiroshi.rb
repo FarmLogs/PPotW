453ffa40d8781d195f523585a0f6ac0dbf621171
Hiroshi Fukada <hiroshi@fukada.us>
2015-10-22 23:59:28 -0400

    it's beautiful

--- 8-Parens/solutions/hiroshi.rb
+++ 8-Parens/solutions/hiroshi.rb
@@ -1,10 +1,9 @@
 def eat(a, char)
-  {'['=>a+7,']'=>a-7,'('=>a*41,')'=>a/41,'{'=>2**a,'}'=>Math.log2(a)}[char]
+  {91=>a+7,93=>a-7,40=>a*41,41=>a/41,123=>2**a,125=>Math.log2(a)}[char]
 end
-
 def wow(r)
-  ->s{s.gsub(/\(|\)|\{|\}|\[|\]/,'').empty? ? s.split(//).inject(s.length){|accumulator, char| eat(accumulator, char)} == s.length : false}\
-    [r.gsub(/('.*?')/,"")]
+  ->s{!s.gsub(/#{[40,124,41,124,123,124,125,124,91,124,93].pack('c'*11)}/,String.new)[/./] ? s.unpack('c'*s.length).inject(s.length){|accumulator, char| eat(accumulator, char)} == s.length : false}\
+    [r.gsub(/#{[39,46,42,63,39].pack('C',5)}/,String.new)]
 end
 
 puts "true: #{wow("()()()([')'][])")}"
