f7e8a9d54988f70a2b9ea53730e9eaf7143b14ea
Hiroshi Fukada <hiroshi@fukada.us>
2015-11-07 22:36:21 -0500

    almost

--- 10-Words/solutions/doit.rb
+++ 10-Words/solutions/doit.rb
@@ -7,4 +7,26 @@ File.open("dict.txt") do |f|
   end
 end
 
+def find_word(charseq):
+  for i in 0..charset.size-1 do
+    potential_word = charseq[0..i]
+    if dict.include? potential_word
+      return potential_word, charseq[i+1..-1]
+  end
+  return nil
+end
 
+def find_all_words(input_str)
+  found_words = Set.new
+  input_str.split('').permutations{|p|
+    temp_sets = []
+    iterator = p.join
+    while (found = find_word(iterator))
+      temp_sets.push(found.first)
+      iterator = found.second
+    end
+    temp_sets.reduce([]){|c, n|
+      found_words.add(Set.new())
+    }
+  }
+end
